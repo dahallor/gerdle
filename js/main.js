cdb1df5a6f1cd04de7dbd0d0b5fe3b3791545ed5
David A. Halloran <d.a.halloran@gmail.com>
2022-03-22 15:05:08 -0400

    Functional by click only

--- js/main.js
+++ js/main.js
@@ -5,22 +5,24 @@ document.addEventListener("DOMContentLoaded", () => {
     squares.createSquares();
     
     var letters = document.querySelectorAll('.keyboard-row button');
+    var keys = document.querySelectorAll('.keyboard-row button')
 
     squares.setCharArray(words);
 
 
-
-
     for(let i = 0; i < letters.length; i++){
-        letters.addEventListener("click", () => {
-            handles.handleInput(letters[i], words, squares);
-        })
-        letters.removeEventLister();
-        letters.addEventListener("keydown", () => {
-            handles.handleInput(letters[i], words, squares);
-        })
-        letters.removeEventLister()
-
+        letters[i].onclick = ({ target }) => {
+            handles.handleInput(target, words, squares);
+        }
     }
+    
+    for(let key of keys){
+        key.onkeydown = ({ target }) => {
+            console.log(target)
+            handles.handleInput(target, words, squares);
+        }
+    }
+
+    
 
 })
