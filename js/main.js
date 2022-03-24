7fd680b9491b70bc40de47528b435c840218f873
David A. Halloran <d.a.halloran@gmail.com>
2022-03-23 22:13:59 -0400

    come back to this

--- js/main.js
+++ js/main.js
@@ -5,23 +5,32 @@ document.addEventListener("DOMContentLoaded", () => {
     squares.createSquares();
     
     var letters = document.querySelectorAll('.keyboard-row button');
-    var keys = document.querySelectorAll('.keyboard-row button')
+    /*
+    var keys = document.getElementById("keyboard-container")
+
+    keys.addEventListener("keypress", e => {
+        handles.handleInput(e.key, words, squares)
+    })*/
+    
 
     squares.setCharArray(words);
 
+    function sendLetter(letter, words, squares, handles){
+        handles.handleInput(letter, words, squares)
+    }
 
-    for(let i = 0; i < letters.length; i++){
+
+    for(let i = 0; i < letters.length; i++){/*
         letters[i].onclick = ({ target }) => {
+            handles.handleInput(target, words, squares);
+        }*/
+        letters[i].onkeypress = ({ target }) => {
+            console.log(letters[i])
             handles.handleInput(target, words, squares);
         }
     }
     
-    for(let key of keys){
-        key.onkeydown = ({ target }) => {
-            console.log(target)
-            handles.handleInput(target, words, squares);
-        }
-    }
+
 
     
 
