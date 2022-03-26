624d81361d39a7e99a2c7b9c7226f6d3a6543951
David A. Halloran <d.a.halloran@gmail.com>
2022-03-26 03:14:04 -0400

    started help modal

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+class Modal{
+    constructor(){
+        this.help = <p>Welcome to Gerdle! Similair to the Wordle game you all know and Love but with a few twists\n\n 
+        
+        First off, instead of 6 guess to figure out a 5 letter english word, you have 6 guesses to figure out a 27 letter german word!\n\n
+        
+        Don't speak German? Well, neither do I. So if you need to know what words are accepted, click the 'list' icon in the upper left to bring a dropdown of potential words\n\need
+        
+        Secondly, the color scheme is changed. Instead of gray for a letter not in the word, gold for a letter in the wrong place, and green for correct place; it is now black for letter not in word, yellow for wrong place but in word, and red for letter in correct place.\n\need
+        
+        Why these colors? Cause it's the german flag silly!\n\n
+        
+        And that's about it! Go forth and play a gratitously difficult version of Wordle!
+        
+        *Note: some browsers seem to have issues with typing. If this happens try selecting a key by mouse click and then start typing*</p>
+    }
+
+
+}
