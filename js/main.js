13ca600b1b7377447309c9548bd1b1e992bf4f20
David A. Halloran <d.a.halloran@gmail.com>
2022-03-26 16:43:22 -0400

    fixed modal issue
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     var words = new Words();
-    var handles = new Handles(words);
+    var handles = new Handles();
     var squares = new Squares();
+    var modal = new Modals(words);
     var keys = new Keyboard();
+
 
     words.setSolution();
     squares.createSquares();
     squares.setCharArray(words);
+    modal.help.addEventListener("click", () => {
+        console.log("debug")
+        modal.outer_modal.style = `background: rgba(0, 0, 0, .5);z-index: 2;`;
+        modal.gameBoard.style = `opacity: 0;`;
+        modal.inner_modal.style = `background: rgba(217, 217, 214, 1);z-index:3;`;
+        modal.modal_content.innerHTML = modal.helpText;
+    });
+
+    modal.x.addEventListener("click", () => {
+        console.log("debug")
+        modal.outer_modal.style = `background: "rgba(0, 0, 0, 0);z-index: 0;`;
+        modal.inner_modal.style = `background: rgba(217, 217, 214, 0);z-index:0;`;
+        modal.gameBoard.style = `opacity: 1;`
+        modal.modal_content.innerHTML = "";
+    });
+    
 
 /*
     for(let i = 0; i < 30; i++){
--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,31 @@
-class Modal{
-    constructor(){
-        this.help = <p>Welcome to Gerdle! Similair to the Wordle game you all know and Love but with a few twists\n\n 
-        
-        First off, instead of 6 guess to figure out a 5 letter english word, you have 6 guesses to figure out a 27 letter german word!\n\n
-        
-        Don't speak German? Well, neither do I. So if you need to know what words are accepted, click the 'list' icon in the upper left to bring a dropdown of potential words\n\need
-        
-        Secondly, the color scheme is changed. Instead of gray for a letter not in the word, gold for a letter in the wrong place, and green for correct place; it is now black for letter not in word, yellow for wrong place but in word, and red for letter in correct place.\n\need
-        
-        Why these colors? Cause it's the german flag silly!\n\n
-        
-        And that's about it! Go forth and play a gratitously difficult version of Wordle!
-        
-        *Note: some browsers seem to have issues with typing. If this happens try selecting a key by mouse click and then start typing*</p>
+class Modals{
+    constructor(words){
+        this.outer_modal = document.getElementById("modal");
+        this.inner_modal = document.getElementById("modal__inner");
+        this.modal_content = document.getElementById("modal__content");
+        this.dict = document.getElementById("dictionary");
+        this.help = document.getElementById("help");
+        this.stats = document.getElementById("stats");
+        this.x = document.getElementById("close-x");
+        this.gameBoard = document.getElementById("board-container");
+        this.helpText = "Welcome to Gerdle! Similair to the Wordle game you all know and Love but with a few twists\n\nFirst off, instead of 6 guess to figure out a 5 letter english word, you have 6 guesses to figure out a 27 letter german word!\n\nDon't speak German? Well, neither do I. So if you need to know what words are accepted, click the 'list' icon in the upper left to bring a dropdown of potential words\n\nSecondly, the color scheme is changed. Instead of gray for a letter not in the word, gold for a letter in the wrong place, and green for correct place; it is now black for letter not in word, yellow for wrong place but in word, and red for letter in correct place.\n\nWhy these colors? Cause it's the german flag silly!\n\nAnd that's about it! Go forth and play a gratitously difficult version of Wordle!\n*Note: some browsers seem to have issues with typing. If this happens try selecting a key by mouse click and then start typing*"
+        this.statsText = "Placeholder";
+        this.dictText = words.wordBank
+    }
+
+
+    openModal(message){
+        console.log("open")
+        this.outer_modal.style = `background: "rgba(0, 0, 0, .5); z-index: 1;`;
+        this.inner_modal.style = `background: rgba(217, 217, 214, 1);`;
+        this.modal_content.innerHTML = message;
+    }
+
+    closeModal(){
+        console.log("close")
+        this.outer_modal.style = `background: "rgba(0, 0, 0, 0); z-index: -1`;
+        this.inner_modal.style = `background: rgba(217, 217, 214, 0);`;
+        this.modal__content.innerHTML("");
     }
 
 
